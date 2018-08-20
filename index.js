c4e5e736eda661e401e78d56cdd00fa24cf01b63
Joseph Werle <joseph.werle@gmail.com>
2018-08-20 10:37:42 -0400

    refactor(index.js): Add type checks and clean up lint issues

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const web3 = require('./web3')
 
 module.exports = context
@@ -7,15 +5,23 @@ module.exports = context
 /**
  * Creates an ARA context that wraps useful
  * instances like `Web3'
+ *
  * @public
  * @default
  * @param {Object} opts
  * @return {Object}
  */
 function context(opts) {
-  if (null == opts) { opts = {} }
+  if (null === opts || (opts && 'object' !== typeof opts)) {
+    throw new TypeError('Expecting options to be an object.')
+  } else if ('object' !== typeof opts) {
+    // eslint-disable-next-line no-param-reassign
+    opts = {}
+  }
+
   const ctx = {
     web3: web3.load(opts.web3)
   }
+
   return ctx
 }
